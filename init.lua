a2f8ba93a464efcbaa44ccdecfa1f3c4c74c59ff
masterboxer <sriharish74@gmail.com>
2023-11-22 11:58:19 +0530

    Added Float Option for Lazygit Terminal

--- init.lua
+++ init.lua
@@ -193,7 +193,25 @@ require('lazy').setup({
     'akinsho/toggleterm.nvim',
     config = function()
       local Terminal = require('toggleterm.terminal').Terminal
-      local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+      local lazygit  = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        dir = "git_dir",
+        direction = "float",
+
+        float_opts = {
+          border = "double",
+        },
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+        end,
+
+        -- function that runs on closing the terminal
+        on_close = function()
+          vim.cmd("startinsert!")
+        end,
+      })
 
       function _G._lazygit_toggle()
         lazygit:toggle()
