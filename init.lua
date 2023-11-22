82029b69004067e14506ed3956de0479ce794858
masterboxer <sriharish74@gmail.com>
2023-11-22 12:56:32 +0530

    Added Back Comment.nvim

--- init.lua
+++ init.lua
@@ -112,7 +112,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
+  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -183,6 +183,9 @@ require('lazy').setup({
     main = 'ibl',
     opts = {},
   },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- For learning vim motions
   {
