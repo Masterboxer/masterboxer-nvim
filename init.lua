7bf8a47d1463996a8ab925d7f47ece53813cad0b
masterboxer <sriharish74@gmail.com>
2023-12-07 17:49:28 +0530

    Line Endings Conversion

--- init.lua
+++ init.lua
@@ -423,6 +423,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+-- Change line endings to unix by default anytime a new buffer is opened.
+vim.cmd([[autocmd BufRead,BufNewFile * set ff=unix]])
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
