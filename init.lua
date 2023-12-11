ea6765bd6b82aef96fa213c541b9c7102431e60c
sriharish_74 <sriharish@sellowpay.com>
2023-12-11 09:57:50 +0530

    Fixed Line Ending Issue

--- init.lua
+++ init.lua
@@ -410,8 +410,9 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
--- Change line endings to unix by default anytime a new buffer is opened.
+-- Change line endings to unix by default anytime a new buffer is read.
 -- vim.cmd([[autocmd BufRead,BufNewFile * set ff=unix]])
+vim.cmd([[autocmd BufRead * set ff=unix]])
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
