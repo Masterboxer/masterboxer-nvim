5432a91d418ae4c329103ce07601e937e270e09c
sriharish-oasys <sriharish@oasyscybernetics.in>
2024-01-19 12:35:30 +0530

    Removed Unused Lualine Configurations

--- init.lua
+++ init.lua
@@ -195,10 +195,13 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
+      },
+      sections = {
+        lualine_x = {'filetype'}
       },
     },
   },
