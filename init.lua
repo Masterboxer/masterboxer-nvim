b1fb55d34ddb5aa518b90f34c09330c0ad404597
sriharish-oasys <sriharish@oasyscybernetics.in>
2024-02-16 11:32:18 +0530

    Added Spectre Package

--- init.lua
+++ init.lua
@@ -44,6 +44,13 @@ require('lazy').setup({
     end
   },
 
+  {
+    'nvim-pack/nvim-spectre',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    }
+  },
+
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
@@ -428,6 +435,20 @@ vim.keymap.set('n', '<leader>ba', "<cmd>:%bd<CR>", { desc = 'Delete All Buffers'
 -- Delete all buffers
 vim.keymap.set('n', '<leader>dm', "<cmd>:delmarks!<CR>:delmarks A-Z0-9<CR>", { desc = 'Delete All Marks' })
 
+-- Spectre Related Keymaps
+vim.keymap.set('n', '<leader>ss', '<cmd>lua require("spectre").toggle()<CR>', {
+    desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+    desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+    desc = "Search on current file"
+})
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
@@ -524,15 +545,20 @@ vim.keymap.set("n", "<leader>gm", "<cmd>Git blame<CR>",
   { noremap = true, silent = true, desc = "Toggle [F]ull Length Git Blame" })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
+
+-- vim.keymap.set('n', '<leader>q', function()
+--   require('telescope.builtin').lsp_document_diagnostics({
+--     initial_mode = "normal",
+--   })
+-- end, { desc = '[S]earch [C]urrent [D]iagnostics' })
 
 vim.keymap.set('n', '<leader>q', function()
   require('telescope.builtin').diagnostics({
     initial_mode = "normal",
   })
-end, { desc = '[S]earch [D]iagnostics' })
+end, { desc = '[S]earch [A]ll [D]iagnostics' })
 
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
 
