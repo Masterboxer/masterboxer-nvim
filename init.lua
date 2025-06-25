402b6c12451e289e3154efde53e7c41430ff1ba5
Masterboxer <sriharish74@gmail.com>
2025-06-26 00:47:16 +0530

    Added Flutter Tools

--- init.lua
+++ init.lua
@@ -281,6 +281,17 @@ require('lazy').setup({
     },
   },
 
+
+{
+  'nvim-flutter/flutter-tools.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim',
+  },
+  config = true,
+},
+
   {
     'nvimtools/none-ls.nvim',
     config = function()
